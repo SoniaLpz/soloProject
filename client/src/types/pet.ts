15556fe8d7ee47fadf647b42c8131a4a8a104c0b
Vitoria Ferraz Lozado <vilozado@gmail.com>
2026-03-05 13:26:20 -0300

    Refactor: App
    Change jsx files to tsx to remove errors.
    Add id to Pet interface.

--- client/src/types/pet.ts
+++ client/src/types/pet.ts
@@ -1,19 +1,19 @@
 export interface Pet {
-    name: string; 
-    type: string;
-    favorite: boolean; 
-    breed?: string; 
-    age?: number ; 
-    gender?: string; 
-    location? : {
-    lat: number,
-    lng: number
-    }; 
-    city?: string; 
-    description?: string, 
-    image?: string, 
-    available?: boolean, 
-    shelterName?: string, 
-    email?: string, 
+  _id: string;
+  name: string;
+  type: string;
+  favorite: boolean;
+  breed?: string;
+  age?: number;
+  gender?: string;
+  location?: {
+    lat: number;
+    lng: number;
+  };
+  city?: string;
+  description?: string;
+  image?: string;
+  available?: boolean;
+  shelterName?: string;
+  email?: string;
 }
-
