2e34bfa8cd24a2dbeb4dd169b25805248bbf2fe4
Vitoria Ferraz Lozado <vilozado@gmail.com>
2026-03-05 10:42:44 -0300

    Refactor to TS: auth middleware

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
-import PetListPage from "./pages/PetListPage";
-import PetDetailPage from "./pages/PetDetailPage";
-import HomePage from "./pages/HomePage";
-import ContactPage from "./pages/ContactPage";
-import AdminDashboard from "./pages/AdminDashboard";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import PetListPage from "./pages/PetListPage.jsx";
+import PetDetailPage from "./pages/PetDetailPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
+import AdminDashboard from "./pages/AdminDashboard.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 import "./App.css";
-import AdminEditPage from "./pages/AdminEditPage";
-import FavoritePetsPage from "./pages/FavsPetPage";
+import AdminEditPage from "./pages/AdminEditPage.jsx";
+import FavoritePetsPage from "./pages/FavsPetPage.jsx";
 
 function App() {
   return (
