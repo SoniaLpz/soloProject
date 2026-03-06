002d15eabedc4a559f5468ec9a8f024cac3b9ff6
Vitoria Ferraz Lozado <vilozado@gmail.com>
2026-03-06 10:55:17 -0300

    Unit tests: authController

--- server/controllers/authController.test.ts
+++ server/controllers/authController.test.ts
@@ -2,14 +2,39 @@ import { describe, test, expect, vi, beforeEach, Mock } from "vitest";
 import { login, register } from "./authController";
 import User from "../models/user";
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 vi.mock("../models/user");
+vi.mock("jsonwebtoken");
+vi.mock("bcrypt");
+
+const mockUser = {
+  _id: "1",
+  role: "adopter",
+  email: "test@example.com",
+  password: "hashedpassword",
+};
+
+const mockRes = () =>
+  ({
+    status: vi.fn().mockReturnThis(),
+    json: vi.fn(),
+  }) as unknown as Response;
 
 describe("Auth Controller", () => {
-  describe.only("Register new user", () => {
-    beforeEach(() => {
-      vi.clearAllMocks();
-    });
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  describe("Register new user", () => {
+    const req = {
+      body: {
+        email: "test@example.com",
+        password: "12345",
+        role: "adopter",
+      },
+    } as Request;
 
     test("returns 201 on successfull registration", async () => {
       (User as unknown as Mock).mockImplementation(function () {
@@ -17,19 +42,8 @@ describe("Auth Controller", () => {
           save: vi.fn().mockResolvedValue({}),
         };
       });
-      const req = {
-        body: {
-          email: "test@example.com",
-          password: "12345",
-          role: "adopter",
-        },
-      } as Request;
 
-      const res = {
-        status: vi.fn().mockReturnThis(),
-        json: vi.fn(),
-      } as unknown as Response;
-
+      const res = mockRes();
       await register(req, res);
 
       expect(res.status).toHaveBeenCalledWith(201);
@@ -45,18 +59,7 @@ describe("Auth Controller", () => {
         };
       });
 
-      const req = {
-        body: {
-          email: "test@example.com",
-          password: "12345",
-          role: "adopter",
-        },
-      } as Request;
-
-      const res = {
-        status: vi.fn().mockReturnThis(),
-        json: vi.fn(),
-      } as unknown as Response;
+      const res = mockRes();
 
       await register(req, res);
 
@@ -68,25 +71,72 @@ describe("Auth Controller", () => {
   });
 
   describe("Login user", () => {
-    test("returns 401 message if no user found", async () => {
-      //arrange
-      //act
-      //assert
+    const req = {
+      body: {
+        email: "test@example.com",
+        password: "hashedpassword",
+      },
+    } as Request;
+
+    beforeEach(() => {
+      process.env.JWT_SECRET = "supersecret";
     });
-    test("returns 401 message if password doesn't match", async () => {
-      //arrange
-      //act
-      //assert
+
+    test("returns token on successful login", async () => {
+      (User.findOne as Mock).mockResolvedValue(mockUser); //returns user object
+      (bcrypt.compare as Mock).mockResolvedValue(true); //checks if password matches
+      (jwt.sign as Mock).mockReturnValue("new.token.here"); //returns token
+
+      const res = mockRes();
+
+      await login(req, res);
+
+      expect(jwt.sign).toHaveBeenCalledWith(
+        { id: mockUser._id, role: mockUser.role },
+        expect.any(String), //secret
+        expect.any(Object), //expiry time
+      );
+      expect(res.json).toHaveBeenCalledWith({
+        token: "new.token.here",
+        role: mockUser.role,
+      });
     });
-    test("generates token if login is successful", async () => {
-      //arrange
-      //act
-      //assert
+    test("returns 401 message if password is incorrect", async () => {
+      (User.findOne as Mock).mockResolvedValue(mockUser); //returns user object
+      (bcrypt.compare as Mock).mockResolvedValue(false); //password does not match
+
+      const res = mockRes();
+
+      await login(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(401);
+      expect(res.json).toHaveBeenCalledWith({
+        error: "Invalid credentials",
+      });
     });
+
+    test("returns 401 message if user not found", async () => {
+      (User.findOne as Mock).mockResolvedValue(null); //returns null for user not found
+
+      const res = mockRes();
+
+      await login(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(401);
+      expect(res.json).toHaveBeenCalledWith({
+        error: "Invalid credentials",
+      });
+    });
+
     test("returns error message if server error", async () => {
-      //arrange
-      //act
-      //assert
+      (User.findOne as Mock).mockRejectedValue(new Error("Server error"));
+      const res = mockRes();
+      await login(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({
+        error: "Server error",
+      });
     });
   });
 });
