7153842919a536d5b07aec746d693cba239c4c82
Sonia Lopez <s.lopez@montpellier-bs.com>
2026-03-11 14:36:39 +0100

    Add Test

--- client/cypress - Sonia/e2e/spec.cy.ts
+++ client/cypress - Sonia/e2e/spec.cy.ts
@@ -1,3 +1,4 @@
+
 describe('My First Test', () => {
   it('Visits the link', () => {
     cy.visit('http://localhost:5173/')
@@ -6,9 +7,36 @@ describe('My First Test', () => {
     cy.location('pathname').should('match', /\/pets/ )
   })
 
-  it('Browse Pets', () => {
-    cy.visit('http://localhost:5173/pets')
-
+  it('List Pets', () => {
+    cy.visit('http://localhost:5173/pets');
+    cy.get('.pet-list').should('contain', 'Buddy')
   })
 
+   it('Maps List Pets', () => {
+    cy.visit('http://localhost:5173/pets');
+    cy.get('h3').should('contain', 'Buddy')
+  })
+  
+    it('Login', () => {
+      cy.visit('http://localhost:5173/login');
+      cy.get('[placeholder = Email]').type('soloProject@gmail.com')
+      cy.get('[placeholder = Password]').type('password26')
+      cy.get('button').click()
+    })
+
+    it('Register', () => {
+      cy.visit('http://localhost:5173/register');
+      cy.get('[placeholder = Email]').type('soloProject@gmail.com')
+      cy.get('[placeholder = Password]').type('password26')
+      cy.get('#role').select('Shelter')
+      cy.get('button').click()
+    })
+
+     it('Contact Us', () => {
+      cy.visit('http://localhost:5173/contact');
+      cy.get('[placeholder = "Your Name"]').type('Lopez')
+      cy.get('[placeholder = "Your Email"]').type('soloProject@gmail.com')
+      cy.get('[placeholder = "Your Message"]').type('Hola')
+      cy.get('button').click()
+    })
 })
